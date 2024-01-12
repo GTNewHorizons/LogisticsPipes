1ba7536fa716e9ff08991652a2547d595b20420c
Glease <4586901+Glease@users.noreply.github.com>
2024-01-13 05:22:40 +0800

    add quantum chest support (#29)
    Co-authored-by: Martin Robertz <dream-master@gmx.net>

--- src/main/java/logisticspipes/proxy/specialinventoryhandler/DSUInventoryHandler.java
+++ src/main/java/logisticspipes/proxy/specialinventoryhandler/DSUInventoryHandler.java
@@ -1,20 +1,14 @@
 package logisticspipes.proxy.specialinventoryhandler;
 
-import java.util.HashMap;
-import java.util.Set;
-import java.util.TreeSet;
-
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.util.ForgeDirection;
 
-import logisticspipes.utils.item.ItemIdentifier;
 import powercrystals.minefactoryreloaded.api.IDeepStorageUnit;
 
-public class DSUInventoryHandler extends SpecialInventoryHandler {
+public class DSUInventoryHandler extends DSULikeInventoryHandler {
 
     private final IDeepStorageUnit _tile;
-    private final boolean _hideOnePerStack;
 
     private DSUInventoryHandler(TileEntity tile, boolean hideOnePerStack, boolean hideOne, int cropStart, int cropEnd) {
         _tile = (IDeepStorageUnit) tile;
@@ -43,139 +37,37 @@ public class DSUInventoryHandler extends SpecialInventoryHandler {
     }
 
     @Override
-    public int itemCount(ItemIdentifier itemIdent) {
-        ItemStack items = _tile.getStoredItemType();
-        if (items != null && ItemIdentifier.get(items).equals(itemIdent)) {
-            return items.stackSize - (_hideOnePerStack ? 1 : 0);
-        }
-        return 0;
+    boolean isEmpty() {
+        return _tile.getStoredItemType() != null;
     }
 
     @Override
-    public ItemStack getMultipleItems(ItemIdentifier itemIdent, int count) {
-        ItemStack items = _tile.getStoredItemType();
-        if (items == null || !ItemIdentifier.get(items).equals(itemIdent)) {
-            return null;
-        }
-        if (_hideOnePerStack) {
-            items.stackSize--;
-        }
-        if (count >= items.stackSize) {
-            _tile.setStoredItemCount((_hideOnePerStack ? 1 : 0));
-            return items;
-        }
-        ItemStack newItems = items.splitStack(count);
-        _tile.setStoredItemCount(items.stackSize + (_hideOnePerStack ? 1 : 0));
-        return newItems;
+    int getSize() {
+        return _tile.getMaxStoredCount();
     }
 
     @Override
-    public Set<ItemIdentifier> getItems() {
-        Set<ItemIdentifier> result = new TreeSet<>();
-        ItemStack items = _tile.getStoredItemType();
-        if (items != null && items.stackSize > 0) {
-            result.add(ItemIdentifier.get(items));
-        }
-        return result;
+    int getCurrent() {
+        return isEmpty() ? 0 : _tile.getStoredItemType().stackSize;
     }
 
     @Override
-    public HashMap<ItemIdentifier, Integer> getItemsAndCount() {
-        HashMap<ItemIdentifier, Integer> result = new HashMap<>();
-        ItemStack items = _tile.getStoredItemType();
-        if (items != null && items.stackSize > 0) {
-            result.put(ItemIdentifier.get(items), items.stackSize - (_hideOnePerStack ? 1 : 0));
-        }
-        return result;
-    }
-
-    @Override
-    public ItemStack getSingleItem(ItemIdentifier itemIdent) {
-        return getMultipleItems(itemIdent, 1);
-    }
-
-    @Override
-    public boolean containsUndamagedItem(ItemIdentifier itemIdent) {
-        ItemStack items = _tile.getStoredItemType();
-        return items != null && ItemIdentifier.get(items).getUndamaged().equals(itemIdent);
-    }
-
-    @Override
-    public int roomForItem(ItemIdentifier item) {
-        return roomForItem(item, 0);
-    }
-
-    @Override
-    public int roomForItem(ItemIdentifier itemIdent, int count) {
-        if (itemIdent.tag != null) {
-            return 0;
-        }
-        ItemStack items = _tile.getStoredItemType();
-        if (items == null) {
-            return _tile.getMaxStoredCount();
-        }
-        if (ItemIdentifier.get(items).equals(itemIdent)) {
-            return _tile.getMaxStoredCount() - items.stackSize;
-        }
-        return 0;
-    }
-
-    @Override
-    public ItemStack add(ItemStack stack, ForgeDirection from, boolean doAdd) {
-        ItemStack st = stack.copy();
-        st.stackSize = 0;
-        if (stack.getTagCompound() != null) {
-            return st;
-        }
-        ItemStack items = _tile.getStoredItemType();
-        if ((items == null || items.stackSize == 0)) {
-            if (stack.stackSize <= _tile.getMaxStoredCount()) {
-                _tile.setStoredItemType(stack, stack.stackSize);
-                st.stackSize = stack.stackSize;
-                return st;
-            } else {
-                _tile.setStoredItemType(stack, _tile.getMaxStoredCount());
-                st.stackSize = _tile.getMaxStoredCount();
-                return st;
-            }
-        }
-        if (!items.isItemEqual(stack)) {
-            return st;
-        }
-        if (stack.stackSize <= _tile.getMaxStoredCount() - items.stackSize) {
-            _tile.setStoredItemCount(items.stackSize + stack.stackSize);
-            st.stackSize = stack.stackSize;
-            return st;
-        } else {
-            _tile.setStoredItemCount(_tile.getMaxStoredCount());
-            st.stackSize = _tile.getMaxStoredCount() - items.stackSize;
-            return st;
-        }
-    }
-
-    @Override
-    public boolean isSpecialInventory() {
-        return true;
-    }
-
-    @Override
-    public int getSizeInventory() {
-        return 1;
-    }
-
-    @Override
-    public ItemStack getStackInSlot(int i) {
-        if (i != 0) {
-            return null;
-        }
+    ItemStack getType() {
         return _tile.getStoredItemType();
     }
 
     @Override
-    public ItemStack decrStackSize(int i, int j) {
-        if (i != 0) {
-            return null;
-        }
-        return getMultipleItems(ItemIdentifier.get(_tile.getStoredItemType()), j);
+    void setContent(int count) {
+        _tile.setStoredItemCount(count);
+    }
+
+    @Override
+    void setContent(ItemStack stack, int size) {
+        _tile.setStoredItemType(stack, size);
+    }
+
+    @Override
+    void markDirty() {
+        ((TileEntity) _tile).markDirty();
     }
 }
---
+++ src/main/java/logisticspipes/proxy/specialinventoryhandler/QuantumChestInventoryHandler.java
@@ -0,0 +1,82 @@
+package logisticspipes.proxy.specialinventoryhandler;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.util.ForgeDirection;
+
+import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
+import gregtech.common.tileentities.storage.GT_MetaTileEntity_QuantumChest;
+
+public class QuantumChestInventoryHandler extends DSULikeInventoryHandler {
+
+    private final GT_MetaTileEntity_QuantumChest _mte;
+
+    private QuantumChestInventoryHandler(GT_MetaTileEntity_QuantumChest tile, boolean hideOnePerStack, boolean hideOne,
+            int cropStart, int cropEnd) {
+        _mte = tile;
+        _hideOnePerStack = hideOnePerStack || hideOne;
+    }
+
+    public QuantumChestInventoryHandler() {
+        _mte = null;
+        _hideOnePerStack = false;
+    }
+
+    @Override
+    public boolean init() {
+        return true;
+    }
+
+    @Override
+    public boolean isType(TileEntity tile) {
+        if (!(tile instanceof IGregTechTileEntity)) return false;
+        return ((IGregTechTileEntity) tile).getMetaTileEntity() instanceof GT_MetaTileEntity_QuantumChest;
+    }
+
+    @Override
+    public SpecialInventoryHandler getUtilForTile(TileEntity tile, ForgeDirection dir, boolean hideOnePerStack,
+            boolean hideOne, int cropStart, int cropEnd) {
+        return new QuantumChestInventoryHandler(
+                ((GT_MetaTileEntity_QuantumChest) ((IGregTechTileEntity) tile).getMetaTileEntity()),
+                hideOnePerStack,
+                hideOne,
+                cropStart,
+                cropEnd);
+    }
+
+    @Override
+    boolean isEmpty() {
+        return _mte.mItemCount == 0 || _mte.mItemStack == null;
+    }
+
+    @Override
+    int getSize() {
+        return _mte.getMaxItemCount();
+    }
+
+    @Override
+    int getCurrent() {
+        return _mte.mItemCount;
+    }
+
+    @Override
+    ItemStack getType() {
+        return _mte.mItemStack;
+    }
+
+    @Override
+    void setContent(int count) {
+        _mte.mItemCount = count;
+    }
+
+    @Override
+    void setContent(ItemStack stack, int size) {
+        _mte.mItemStack = stack.copy();
+        _mte.mItemCount = size;
+    }
+
+    @Override
+    void markDirty() {
+        _mte.markDirty();
+    }
+}
