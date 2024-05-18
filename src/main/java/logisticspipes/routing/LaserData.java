e9f417e282e20c2dae06fd1b9bc1f6d0b1014d16
Glease <4586901+Glease@users.noreply.github.com>
2024-05-18 11:54:55 +0800

    revert because lombok

--- src/main/java/logisticspipes/routing/LaserData.java
+++ src/main/java/logisticspipes/routing/LaserData.java
@@ -20,10 +20,13 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 public class LaserData {
 
+    @NonNull
     private int posX;
 
+    @NonNull
     private int posY;
 
+    @NonNull
     private int posZ;
 
     @NonNull
