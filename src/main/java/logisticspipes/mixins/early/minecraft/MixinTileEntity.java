0daf21e0b83c2b55c620d5f8e0e7f61a34d26646
glowredman <35727266+glowredman@users.noreply.github.com>
2025-05-15 22:51:18 +0200

    Move methods from hooks class to mixin classes

--- src/main/java/logisticspipes/mixins/early/minecraft/MixinTileEntity.java
+++ src/main/java/logisticspipes/mixins/early/minecraft/MixinTileEntity.java
@@ -8,9 +8,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import logisticspipes.asm.LogisticsASMHookClass;
+import logisticspipes.LPConstants;
 import logisticspipes.asm.te.ILPTEInformation;
 import logisticspipes.asm.te.LPTileEntityObject;
+import logisticspipes.routing.pathfinder.changedetection.TEControl;
 
 @Mixin(TileEntity.class)
 public class MixinTileEntity implements ILPTEInformation {
@@ -20,12 +21,26 @@ public class MixinTileEntity implements ILPTEInformation {
 
     @Inject(at = @At("HEAD"), method = "invalidate")
     private void logisticspipes$invalidate(CallbackInfo ci) {
-        LogisticsASMHookClass.invalidate((TileEntity) (Object) this);
+        try {
+            TEControl.invalidate((TileEntity) (Object) this);
+        } catch (Exception e) {
+            if (LPConstants.DEBUG) {
+                throw e;
+            }
+            e.printStackTrace();
+        }
     }
 
     @Inject(at = @At("HEAD"), method = "validate")
     private void logisticspipes$validate(CallbackInfo ci) {
-        LogisticsASMHookClass.validate((TileEntity) (Object) this);
+        try {
+            TEControl.validate((TileEntity) (Object) this);
+        } catch (Exception e) {
+            if (LPConstants.DEBUG) {
+                throw e;
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
--- src/main/java/logisticspipes/mixins/early/minecraft/MixinWorld.java
+++ src/main/java/logisticspipes/mixins/early/minecraft/MixinWorld.java
@@ -8,7 +8,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import logisticspipes.asm.LogisticsASMHookClass;
+import logisticspipes.LPConstants;
+import logisticspipes.routing.pathfinder.changedetection.TEControl;
 
 @Mixin(World.class)
 public class MixinWorld {
@@ -16,20 +17,39 @@ public class MixinWorld {
     @Inject(at = @At("HEAD"), method = "notifyBlocksOfNeighborChange(IIILnet/minecraft/block/Block;)V")
     private void logisticspipes$notifyBlocksOfNeighborChange_Start(int p_147459_1_, int p_147459_2_, int p_147459_3_,
             Block p_147459_4_, CallbackInfo ci) {
-        LogisticsASMHookClass
-                .notifyBlocksOfNeighborChange_Start((World) (Object) this, p_147459_1_, p_147459_2_, p_147459_3_);
+        try {
+            TEControl.notifyBlocksOfNeighborChange_Start((World) (Object) this, p_147459_1_, p_147459_2_, p_147459_3_);
+        } catch (Exception e) {
+            if (LPConstants.DEBUG) {
+                throw e;
+            }
+            e.printStackTrace();
+        }
     }
 
     @Inject(at = @At("TAIL"), method = "notifyBlocksOfNeighborChange(IIILnet/minecraft/block/Block;)V")
     private void logisticspipes$notifyBlocksOfNeighborChange_Stop(int p_147459_1_, int p_147459_2_, int p_147459_3_,
             Block p_147459_4_, CallbackInfo ci) {
-        LogisticsASMHookClass
-                .notifyBlocksOfNeighborChange_Stop((World) (Object) this, p_147459_1_, p_147459_2_, p_147459_3_);
+        try {
+            TEControl.notifyBlocksOfNeighborChange_Stop((World) (Object) this, p_147459_1_, p_147459_2_, p_147459_3_);
+        } catch (Exception e) {
+            if (LPConstants.DEBUG) {
+                throw e;
+            }
+            e.printStackTrace();
+        }
     }
 
     @Inject(at = @At("HEAD"), method = "notifyBlockOfNeighborChange")
     private void logisticspipes$notifyBlockOfNeighborChange(int p_147460_1_, int p_147460_2_, int p_147460_3_,
             final Block p_147460_4_, CallbackInfo ci) {
-        LogisticsASMHookClass.notifyBlockOfNeighborChange((World) (Object) this, p_147460_1_, p_147460_2_, p_147460_3_);
+        try {
+            TEControl.notifyBlockOfNeighborChange((World) (Object) this, p_147460_1_, p_147460_2_, p_147460_3_);
+        } catch (Exception e) {
+            if (LPConstants.DEBUG) {
+                throw e;
+            }
+            e.printStackTrace();
+        }
     }
 }
